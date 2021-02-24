7103b1987689f39d9036d9b002cf57e4057b237a
Kondratiuk Artem <artem.kondratyuk.1995@gmail.com>
2021-02-24 13:30:55 +0200

    fix login

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
@@ -44,22 +43,9 @@ class LoginAuthenticator extends AbstractGuardAuthenticator
 
     public function getCredentials(Request $request)
     {
-        $credentials = [
-            'email' => $request->request->get('email'),
-            'password' => $request->request->get('password'),
-            'csrf_token' => $request->request->get('_csrf_token'),
-        ];
-        $request->getSession()->set(
-            Security::LAST_USERNAME,
-            $credentials['email']
-        );
+        $credentials = $request->getContent();
 
-        return $credentials;
-
-//        return [
-//            'email' => $request->request->get("email"),
-//            'password' => $request->request->get("password")
-//        ];
+        return $credentials = json_decode($credentials, true);
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
